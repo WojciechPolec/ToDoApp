c76cca71889f3a588d3cea5b2cb7a03159d3a876
WojciechPolec <wojciech.polec00@gmail.com>
2020-10-25 20:00:01 +0100

    Updates

---
+++ precache-manifest.7639ae7e5a71bab2bf3e1a3c408bdc4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc210427b695af7b2f2d910de5cf908c",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "8f1ed3807cd0ffa47b9d",
+    "url": "/ToDoApp/static/css/main.1ce84541.chunk.css"
+  },
+  {
+    "revision": "1d4b3c5e7edb13b09cb3",
+    "url": "/ToDoApp/static/js/2.6b6d6fb3.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/ToDoApp/static/js/2.6b6d6fb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f1ed3807cd0ffa47b9d",
+    "url": "/ToDoApp/static/js/main.1e7b15c6.chunk.js"
+  },
+  {
+    "revision": "72c971a6e659867ff43d",
+    "url": "/ToDoApp/static/js/runtime-main.410fda1f.js"
+  }
+]);
